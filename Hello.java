9880defcfec66174bf0b48c0bc5dda2873e0f44b
Rabia Sayim <105115560+RabiaSayim@users.noreply.github.com>
2022-08-06 00:35:32 +0100

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+package gitWorks1;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello,it's big step for me but small step for humanity.. ");
+        System.out.println("Created git Repository");
+        System.out.println("What a sunny day!");
+        System.out.printf("");
+
+
+
+    }
+}
